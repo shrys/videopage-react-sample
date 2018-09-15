e37b2f1c7c5d7448d729e2f75c1e6606fd71c0a8
shrys <shreyas770@yahoo.in>
2018-09-15 17:22:42 +0530

    API call

--- src/components/search-bar.component.js
+++ src/components/search-bar.component.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 
-class SearchBar extends Component {
+class SearchBar extends Component { // states: only class based components have states not funtion based componenets
+
+    constructor(props) {
+        super(props); // ctor from componenets
+
+        this.state = { term: '' }; // whenever user searches something, state will be used
+    }
     render() {
         return (
             <div className="col-md-12 mt-4">
                 <div className="input-group mb-3">
                     <div className="input-group-prepend">
-                        <span className="input-group-text">search here</span>
+                        <span className="input-group-text">videos</span>
                     </div>
-                    <input className="form-control" onChange={this.onIputChange} />
+                    <input value={ this.state.term } className="form-control" onChange={ this.onIputChange } placeholder='search here' />
                 </div>
             </div>
         );
     }
 
     onIputChange(event) {
-        console.log(event.target.value);
-        ;
+        this.setState({ term: event.target.value });
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search-bar.component';
 
 const API_KEY = '';
+
+YTSearch({ key: API_KEY, term: 'surf' }, (data) => {
+    console.log(data);
+    
+});
 
 const App = () => {
     return (
