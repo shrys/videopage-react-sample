8cbbe005434aa4c995e35ad534834580ba8f9aa1
shrys <shreyas770@yahoo.in>
2018-09-15 15:49:41 +0530

    react intro

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import FormContainer from "./js/components/container/FormContainer";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>hi!</div>; // jsx yoo
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
